88853a4aecb1510d7e6ddb7f4dfff7179f439fd0
md kowsir sikder <165156732+ko10wsir@users.noreply.github.com>
2024-04-01 17:06:39 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,7 @@
+alert('hello')
+var typed = new Typed(".typing",{
+    strings:["Web Designer","Web Developer","Css Expart","Js Expart","YouTuber","wordpress Expart","Html Expart"],
+    typeSpeed:100,
+    BackSpeed:80,
+    loop:true
+})
